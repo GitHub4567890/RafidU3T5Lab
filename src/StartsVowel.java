af98ee9c4ea580e9085f166d7f9bfd34015d87ef
Ibtehaz Rafid <IbtehazR@nycstudents.net>
2022-11-01 12:34:43 -0400

    1

--- src/StartsVowel.java
+++ src/StartsVowel.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class StartsVowel {
+import java.util.Scanner;
+
+public class StartsVowel {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+        System.out.print("Enter a lowercase word: ");
+        String word = myScanner.nextLine();
+        String first = word.substring(0, 1);  // get first letter
+
+        if ("aeiou".indexOf(first) != -1) {
+            System.out.println("Your word begins with a vowel");
+        } else {
+            System.out.println("Your word begins with a consonant");
+        }
+
+        myScanner.close();
+    }
 }
